73f25d4475c682f75d0c89d3299c33423b53e481
Wade Jones <wadedjones@gmail.com>
2021-12-24 22:28:53 -0800

    added files

---
+++ linear_regression_scratch.py
@@ -0,0 +1,8 @@
+import numpy as np
+import math
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import random
+
+print("hello world")
